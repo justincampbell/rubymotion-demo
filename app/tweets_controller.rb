f35541675a54365570d7399a6f6349c9af71ee1f
Justin Campbell <justin@justincampbell.me>
2013-02-14 08:58:57 -0500

    Add data to table view, create TweetCell

--- app/tweets_controller.rb
+++ app/tweets_controller.rb
@@ -1,3 +1,13 @@
 class TweetsController < UITableViewController
+  def viewDidLoad
+    @tweets = %w[One Two Three]
+  end
 
+  def tableView(tableView, numberOfRowsInSection: section)
+    @tweets.size
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    TweetCell.fromTweet(@tweets[indexPath.row])
+  end
 end
